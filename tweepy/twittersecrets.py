e231dcc20aad4479e652e6e6154dacf03383b26c
Cam <camnewnham@gmail.com>
2017-10-29 17:14:22 -0400

    Added p2p

---
+++ tweepy/twittersecrets.py
@@ -0,0 +1,7 @@
+# twittersecrets.py
+
+handle = '@newskeee'
+consumer_key = 'rkaUEjobCO3KPNYk8d9Xr46FV'
+consumer_secret = 'kqMkOWZYcl2R3kTZGdIBuxfyQsF2noeKOF2mwhl4VtjXxLkS1A'
+access_token = '875504685049815040-JZpRHhUHpV0lIzB5q1F0zfI2jLZ7Wr4'
+access_secret = 'GD8Gmu2CzFS5hUB8uSLln7M7VJFsR5eCkvwtKLps4zIUK'
