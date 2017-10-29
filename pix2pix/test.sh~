d3bf4f206097dd4f00710a3bd16f536e3b1f535c
Cam <camnewnham@gmail.com>
2017-10-29 17:31:41 -0400

    Added p2p

---
+++ pix2pix/test.sh~
@@ -0,0 +1,1 @@
+python test.py --dataroot ./datasets/$1 --name $2 --model pix2pix --which_model_netG unet_1024 --loadSize 1054 --fineSize 1024 --which_direction BtoA --dataset_mode single --norm batch
---
+++ pix2pix/train.sh
@@ -0,0 +1,1 @@
+python train.py --dataroot ./datasets/$1 --name $2 --model pix2pix --which_model_netG unet_1024 --loadSize 1054 --fineSize 1024 --which_direction BtoA --lambda_A 100 --dataset_mode aligned --no_lsgan --norm batch --pool_size 0 --display_freq 20
